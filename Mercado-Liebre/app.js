86d29b9a0e8503cdefe7c5fc908d1f85021a95eb
Mcsand22 <mabernal94@gmail.com>
2021-10-02 17:41:42 -0300

    agregué estilos

--- Mercado-Liebre/app.js
+++ Mercado-Liebre/app.js
@@ -5,8 +5,8 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
+app.listen(4000, () => {
+    console.log('Servidor corriendo en el puerto 4000');
 });
 
 
