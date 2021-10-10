2715fa9ec8ab4324d57c7ddfe74d3f658e6c9231
Mcsand22 <mabernal94@gmail.com>
2021-10-10 09:40:02 -0300

    edité como proyecto

--- Mercado-Liebre/app.js
+++ Mercado-Liebre/app.js
@@ -5,8 +5,8 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(4000, () => {
-    console.log('Servidor corriendo en el puerto 4000');
+app.listen(5000, () => {
+    console.log('Servidor corriendo en el puerto 5000');
 });
 
 
