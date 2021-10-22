7491ae4c8c480b9a508cdc6c87a27667f0e3e3fe
Mcsand22 <mabernal94@gmail.com>
2021-10-21 21:18:19 -0300

    cambios para heroku

--- Mercado-Liebre/app.js
+++ Mercado-Liebre/app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, function () {
     console.log('Servidor corriendo en el puerto 5000');
 });
 
